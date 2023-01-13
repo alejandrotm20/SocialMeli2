54872c46ef6802e2afa1b0fcb80cc9e567d1366e
alejandrotm20 <edison.torres@mercadolibre.com.co>
2023-01-13 12:19:11 -0500

    Se refactorizan las excepciones y se crea clase ErrorResponseDto

---
+++ be_java_hisp_w20_g6/src/main/java/com/bootcamp/be_java_hisp_w20_g6/dto/response/ErrorResponseDto.java
@@ -0,0 +1,14 @@
+package com.bootcamp.be_java_hisp_w20_g6.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ErrorResponseDto {
+    private String name;
+    private String description;
+}
